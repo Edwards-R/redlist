a7ee75ef8bed032e9c884e3d3abf667de57139da
Rowan <rowan.edw@gmail.com>
2023-01-25 11:11:35 +0000

    Created 'buffer_union' function that handles all the complex stuff. Runs in 22 seconds, so still not fast, but returns a table which can be saved down or (if you're insane) dynamically queried. Probably worth examining the use of temporary tables, but I think that the human 'sessions' will be so long as to make regular tables just better

---
+++ supporting_functions/buffer_union.sql
@@ -0,0 +1,77 @@
+/*
+ * This function:
+ *      selects data from the defined table
+ *      filters it to the prescribed date range (year 0-3000 default)
+ *      translates to a unified datum (27700 as default)
+ *      creates a buffer of size 'distance'
+ *      merges all the resulting buffers into one polygon
+ *
+ *  The function is EXTREMELY resources intensive to run, so avoid calling it dynamically
+ *  Any pre-optimisation that can be done, should be done.
+ *  The function is designed to be used as part of a package that ensures that the data it requires
+ *  is available, formatted, and optimised.
+ *
+ *  There's probably a way to write this as a proper aggregate function, but my brain won't follow it right now
+ *  On top of that, this is being written for now rather than later, so *shrug*
+ */
+
+CREATE OR REPLACE FUNCTION redlist.buffer_union(
+    source_name TEXT, -- The table to use as a data source
+    distance    INT, -- Distance of the buffer
+    start_year  INT DEFAULT 0, -- Lowest year, inclusive
+    end_year    INT DEFAULT 3000, --Higheste year, inclusive
+    datum       INT DEFAULT 27700 -- The datum to unify all geometry to
+)
+RETURNS TABLE (
+    tik INT,
+    poly public.geometry,
+    sq_km INT
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+
+BEGIN
+    SET SCHEMA 'redlist';
+
+    RETURN QUERY EXECUTE FORMAT('
+        WITH raw AS (
+            SELECT tik,
+            easting,
+            northing,
+            accuracy,
+            datum
+            FROM %I
+            WHERE lower_year >= $1
+            AND upper_year <= $2
+            GROUP BY tik, easting, northing, accuracy, datum
+        ),
+
+        b_u AS (
+            SELECT
+            tik,
+            public.ST_UNION(
+                public.ST_BUFFER(
+                    public.ST_TRANSFORM(
+                        redlist.enad_to_poly(
+                            easting,
+                            northing,
+                            accuracy,
+                            datum
+                        ),
+                        27700
+                    ),
+                    40000
+                )
+            ) poly
+            FROM raw
+            GROUP BY tik
+        )
+        
+        SELECT tik, poly, (public.ST_AREA(poly)/1000000)::INT sq_km
+        FROM b_u',
+        source_name, start_year, end_year
+    )
+    USING start_year, end_year;
+END;
+$$;
